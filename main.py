a7f106d19fe9674cb753373f6ac160cd646b8b16
DivyanshKushwaha2023 <divyanshkushwaha2407@gmail.com>
2024-03-05 03:35:46 +0530

    Update ranks list to get rank & value pair of card

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ import random
 
 cards = []
 suits = ["spades", "clubs", "hearts", "diamonds"]
-ranks =["A", "2", "3", "4", "5", "6", "7", "8","9", "10", "J", "Q", "K"]
+ranks =[{"rank": "A", "value": 11},
+        {"rank": "2", "value": 2},
+        {"rank": "3", "value": 3},
+        {"rank": "4", "value": 4},
+        {"rank": "5", "value": 5},
+        {"rank": "6", "value": 6},
+        {"rank": "7", "value": 7},
+        {"rank": "8", "value": 8},
+        {"rank": "9", "value": 9},
+        {"rank": "10", "value": 10},
+        {"rank": "J", "value": 10},
+        {"rank": "Q", "value": 10},
+        {"rank": "K", "value": 10},
+        ]
 for suit in suits:
     for rank in ranks:
         cards.append([suit, rank])
@@ -22,6 +35,7 @@ def deal(number):
 
 
 shuffle()
-cards_dealt = deal(2)
-card = cards_dealt[0]
-print(card)
+
+card = deal(1)[0]
+
+print(card[1]["value"])
